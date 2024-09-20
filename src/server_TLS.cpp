c8eae6b0eae1f45de63ec2cddbba5b76ac00353b
Omeg8051 <131792033+Omeg8051@users.noreply.github.com>
2024-09-21 02:12:36 +0800

    Update server_TLS.cpp
    Tidy up the code and fixed the built-in manual (remove the options for my other related program)

--- src/server_TLS.cpp
+++ src/server_TLS.cpp
@@ -1,6 +1,3 @@
-//#include <opencv2/opencv.hpp>
-
-
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/socket.h>
@@ -17,6 +14,12 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
+/*
+comment this to enable the 'l' command to see client
+keep this if you want to lunch this as an daemon
+*/
+#define DAEMON_LUNCH
+
 
 using namespace std;
 
@@ -27,14 +30,9 @@ const char *chain_path = chain_path_d,*key_path = key_path_d;
 ServerThreadInput server_cfg;
 
 
-//VideoCapture *cam;
-//Mat frm;
 char e;
-//vector<unsigned char> jpg;
-//vector<int> encodectl;
 int *compqval;
 unsigned long ot,pgt;
-//unsigned char fps, fpsd;
 int devpipefd;
 char lkfrm = 0;
 unsigned long tftm[2] = {0,500};
@@ -62,11 +60,6 @@ void sigInt(int){
 int main (int argc,const char **argv){
 	signal(SIGINT,sigInt);
 	
-	//printf("hello world\n");
-	//if(argc <= 1){
-	//	printf("\n");
-	//	return -1;
-	//}
 	//init all used variables from available options like what an actuall useful programe does
 	//this sucks :3
 	int tmp_arg;
@@ -111,8 +104,7 @@ int main (int argc,const char **argv){
 				if(argv[j][1] != 'h'){
 					printf("unknown argument: %c\n",argv[j][1]);
 				}
-				printf("useage:\n%s[ -<c|k|v|d|e|p> <argument>][...]\n\n\tc:\tset <argument> as chain path.\n\tk:\tset <argument> as private key path.\n\tv:\tset <argument> as video device path.\n\td:\tset <argument> as platform device path.\n\te:\tset <argument> as platform device control exe path, or \"echo\" to disable this function\n\tp:\tset <argument> as server port.\n\n(more on the device driver)\nthe driver accepts single charactor commands from the stdin:\n\t{w,s,a,d}:\tmove the platform\n\t{l}:\ttoggle night vision IR lamp\n\t{0,1}:\tAC function; 0->off; 1->on\n",argv[0]);
-
+				printf("useage:\n%s[ -<c|k|p> <argument>][...]\n\n\tc:\tset <argument> as chain path.\n\tk:\tset <argument> as private key path.\n\tp:\tset <argument> as server port.",argv[0]);
 				return 0;
 			break;	
 		}
@@ -120,61 +112,21 @@ int main (int argc,const char **argv){
 
 	//init SSL_CTX
 
-	
-
 	clilst = getClientList();
 	server_cfg.ctx = create_context();
 	configure_context(server_cfg.ctx,chain_path,key_path);
-	
+	//lunch server thread
 	pthread_create(&network,NULL,netFuncAcc,&server_cfg);
 		
 	
-	//oprn camera. The whole point is to be able to use camera. So end prog if failed.
-	/*cam = new VideoCapture(camera_path);
-
-	if(cam == 0){
-		printf("camera %s can't be opened\n",camera_path);
-		e = 'q';
-	} else {
-		//cam->set(CAP_PROP_FORMAT,CAP_PROP_CODEC_PIXEL_FORMAT);
-
-		//for(int i = 0;i<20;i++)
-
-		//open server(envoke network)
-		pthread_create(&network,NULL,netFuncAcc,NULL);
-		encodectl.push_back(IMWRITE_JPEG_QUALITY);
-		encodectl.push_back(50);
-		compqval = encodectl.data();
-		//printf("quality:%d\n",compqval[1]);
-							
-	
-	}
-
-		
-
-	if(mkfifo("hwpipe",0666)){
-		printf("./hwpipe exist or not enough permission to alter file system\n");
-	}
-	printf("opening platform hardware\n");
-		
-	hwpipefd = open("hwpipe",O_RDWR,0666);
-	if(hwpipefd < 0){
-		printf("cant open platform hardware. no platform support\n");
-		hwpipefd = 0;
-	} else {
-		printf("opened platform hardware\n");
-		write(hwpipefd,"x",1);
-		//system("/home/omeg/proj/camPlatctl/platctl /dev/ttyUSB0 -f < hwpipe&");
-		system(buffer[0]);
-	}
-	*/
-
 	while(e != 'q'){
 		usleep(200000);
 		char wha[100];
 		char i;
-		//int cmdl = read(STDIN_FILENO,wha,100);
-		//printf(ANSI_COLOR_YELLOW "[INFO]cmd:%s\n" ANSI_COLOR_RESET,wha);
+		#ifndef DAEMON_LUNCH
+		int cmdl = read(STDIN_FILENO,wha,100);
+		printf(ANSI_COLOR_YELLOW "[INFO]cmd:%s\n" ANSI_COLOR_RESET,wha);
+		#endif
 		switch(wha[0]){
 			case 'l':
 				printf("list clients:{");
@@ -197,6 +149,7 @@ int main (int argc,const char **argv){
 	
 	stopServer();
 	pthread_join(network,NULL);
+	
 	//I keep forgot to wait for all the client thread to join
 	waitAllCliEnd();
 	
